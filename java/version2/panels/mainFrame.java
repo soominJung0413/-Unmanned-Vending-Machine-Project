18031ad53ece33906d24d3d407064ac9f1236e6d
soominJung0413 <doli061214@gmail.com>
2020-11-06 20:11:00 +0900

    MainFrame

--- java/version2/panels/mainFrame.java
+++ java/version2/panels/mainFrame.java
@@ -38,6 +38,12 @@ public class mainFrame extends JFrame {
 		return;
 		// 판넬들을 구별하여 알맞은 필드에 할당하며 마지막으로 해당 판넬을 mainFrame에 등록하는 Setter 입니다. 
 	}
+	
+	public static void PrintTable(String[][] check) {
+		// <TextFields 에서 사용될 PrintTable 입니다. 카트 판넬에 해당 JScrollPane을 등록합니다. printTable()은 CartTable에 있는 메서드로 
+		//매개변수를 받아 테이블을 생성한 뒤 바로 JScrollPane 으로 리턴합니다.
+		cartPanel.addCart(version2.table.CartTable.printTable(check));
+	}
 
 	
 	// 판넬의 UI처리를 한 클래스 내에서 다루기 위해 setter로 주입받은 판넬들을 사용하게 됩니다.
@@ -61,10 +67,5 @@ public class mainFrame extends JFrame {
 		});
 	}
 
-	public static void PrintTable(String[][] check) {
-		// <TextFields 에서 사용될 PrintTable 입니다. 카트 판넬에 해당 JScrollPane을 등록합니다. printTable()은 CartTable에 있는 메서드로 
-		//매개변수를 받아 테이블을 생성한 뒤 바로 JScrollPane 으로 리턴합니다.
-		cartPanel.addCart(version2.table.CartTable.printTable(check));
-	}
 
 }
